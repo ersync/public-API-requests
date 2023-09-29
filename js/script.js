9520a6c2525a3aebeb2fe5f2afd74c2b0c33bd78
Sub Zero <121079771+ersync@users.noreply.github.com>
2023-09-29 22:49:02 +0330

    add modal functionality

--- js/script.js
+++ js/script.js
@@ -23,8 +23,44 @@ function createCard(employee) {
                 </div>
             </div>`
     gallery.insertAdjacentHTML("beforeend", html)
-
+    const card = gallery.lastElementChild
+    card.addEventListener("click", (e) =>
+        createModal(employee)
+    )
 }
+
+function createModal(employee) {
+    const {
+        name,
+        email,
+        picture,
+        location: {city, state, street: {name: streetName, number: streetNumber}, postcode},
+        cell,
+        dob,
+    } = employee
+    const birthday = new Date(dob.date).toLocaleDateString('en-US')
+    let html = `<div class="modal-container">
+                    <div class="modal">
+                        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                        <div class="modal-info-container">
+                            <img class="modal-img" src="${picture.large}" alt="profile picture">
+                            <h3 id="name" class="modal-name cap">${name.first} ${name.last}</h3>
+                            <p class="modal-text">${email}</p>
+                            <p class="modal-text cap">${city}</p>
+                            <hr>
+                            <p class="modal-text">${cell}</p>
+                            <p class="modal-text">${streetNumber} ${streetName}, ${city}, ${state}, ${postcode}</p>
+                            <p class="modal-text">Birthday: ${birthday}</p>
+                        </div>
+                    </div>
+                </div>`
+    gallery.insertAdjacentHTML("beforeend", html)
+    const activeModal = document.querySelector(".modal")
+    activeModal.firstElementChild.addEventListener("click", (e) => {
+        (activeModal).parentElement.remove()
+    })
+}
+
 
 async function showEmployees(url) {
     const employees = await getEmployees(url)
